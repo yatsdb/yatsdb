426e8f72808af7d9aec3a6d1eb028bb57f04209f
niukey <niukey@126.com>
2021-10-15 00:22:26 +0800

    update BadgerIndex matcher

--- inverted-Index/inverted_index.go
+++ inverted-Index/inverted_index.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/dgraph-io/badger/v3"
+	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/prompb"
 	"github.com/sirupsen/logrus"
 	badgerbatcher "github.com/yatsdb/yatsdb/badger-batcher"
@@ -13,9 +14,9 @@ import (
 type StreamID uint64
 
 type StreamMetric struct {
-	Labels []prompb.Label
+	Labels []prompb.Label `json:"labels,omitempty"`
 	//streamID  metric stream
-	StreamID StreamID
+	StreamID StreamID `json:"stream_id,omitempty"`
 	//Offset to read
 }
 
@@ -38,11 +39,13 @@ type BadgerIndex struct {
 }
 
 var (
-	sep = []byte(`\xff`)
+	invertedKeyPrefix = "$0"
+	metricKeyPrefix   = "$1"
+	sep               = []byte(`\xff`)
 )
 
 /*
-key format:
+key format: |$0|sep|lable_name|sep|lable_value|stream_id
 name sep value sep streamIO
 */
 func (index *BadgerIndex) Insert(streamMetric StreamMetric) error {
@@ -50,10 +53,10 @@ func (index *BadgerIndex) Insert(streamMetric StreamMetric) error {
 		var IDBuf = make([]byte, 8)
 		binary.BigEndian.PutUint64(IDBuf, uint64(streamMetric.StreamID))
 		for _, label := range streamMetric.Labels {
-			buf := make([]byte, len(label.Name)+len(label.Value)+len(sep)+8)
+			buf := make([]byte, len(invertedKeyPrefix)+len(sep)+len(label.Name)+len(label.Value)+len(sep)+8)
 			key := buf
 			//copy name
-			n := copy(buf, label.Name)
+			n := copy(buf, invertedKeyPrefix+string(sep)+label.Name)
 			buf = buf[n:]
 			//copy sep
 			n = copy(buf, sep)
@@ -71,6 +74,7 @@ func (index *BadgerIndex) Insert(streamMetric StreamMetric) error {
 				return err
 			}
 		}
+
 		return nil
 	}); err != nil {
 		logrus.Errorf("db update failed %+v", err)
@@ -79,7 +83,24 @@ func (index *BadgerIndex) Insert(streamMetric StreamMetric) error {
 	return nil
 }
 
-func (index *BadgerIndex) Matcher(matcher ...*prompb.LabelMatcher) ([]StreamMetric, error) {
+func (index *BadgerIndex) Matcher(matchers ...*prompb.LabelMatcher) ([]StreamMetric, error) {
+	LabelMatchersSort(matchers)
+	firstMatcher := matchers[0]
+	index.db.View(func(txn *badger.Txn) error {
+		matcher := NewMatcher(*firstMatcher)
+		if matcher.matchEmpty &&
+			(matcher.labelsMatcher.Type == labels.MatchEqual || matcher.labelsMatcher.Type == labels.MatchRegexp) {
+			// l=""
+			// If the matchers for a labelname selects an empty value, it selects all
+			// the series which don't have the label name set too. See:
+			// https://github.com/prometheus/prometheus/issues/3575 and
+			// https://github.com/prometheus/prometheus/pull/3578#issuecomment-351653555
+
+		} else {
+		}
+		return nil
+	})
+
 	return nil, fmt.Errorf("not implemented")
 }
 
