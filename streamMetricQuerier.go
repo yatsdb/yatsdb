d3099810556250265a14bac36bc98300dc65f7c3
niukey <niukey@126.com>
2021-11-01 22:41:39 +0800

    add log for query metric,offset

--- streamMetricQuerier.go
+++ streamMetricQuerier.go
@@ -1,7 +1,10 @@
 package yatsdb
 
 import (
+	"time"
+
 	"github.com/prometheus/prometheus/prompb"
+	"github.com/sirupsen/logrus"
 	invertedindex "github.com/yatsdb/yatsdb/inverted-Index"
 	ssoffsetindex "github.com/yatsdb/yatsdb/ssoffsetindex"
 )
@@ -15,10 +18,17 @@ var _ StreamMetricQuerier = (*streamMetricQuerier)(nil)
 
 func (querier *streamMetricQuerier) QueryStreamMetric(query *prompb.Query) ([]*StreamMetricOffset, error) {
 	var offset []*StreamMetricOffset
+	begin := time.Now()
 	streamMetrics, err := querier.metricMatcher.Matches(query.Matchers...)
 	if err != nil {
 		return nil, err
 	}
+	logrus.WithFields(logrus.Fields{
+		"count":   len(streamMetrics),
+		"elapsed": time.Since(begin),
+	}).Info("matches metrics success")
+	begin = time.Now()
+	//Todo multi goroutine to get offset
 	for _, metric := range streamMetrics {
 		offsetStart, err := querier.streamTimestampOffsetGetter.GetStreamTimestampOffset(metric.StreamID, query.StartTimestampMs, false)
 		if err != nil {
@@ -33,5 +43,9 @@ func (querier *streamMetricQuerier) QueryStreamMetric(query *prompb.Query) ([]*S
 			EndTimestampMs:   query.EndTimestampMs,
 		})
 	}
+	logrus.WithFields(logrus.Fields{
+		"count":   len(streamMetrics),
+		"elapsed": time.Since(begin),
+	}).Info("GetStreamTimestampOffset success")
 	return offset, nil
 }
